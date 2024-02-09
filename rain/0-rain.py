f81602dd4a1725a6f16cad6bc506680e900560be
nkubana0 <i.shema@alustudent>
2024-02-09 02:12:42 -0800

    commit

---
+++ rain/0-rain.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+def rain(walls):
+    if not walls:
+        return 0
+
+    n = len(walls)
+    left_max = [0] * n
+    right_max = [0] * n
+
+
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], walls[i])
+
+    right_max[n - 1] = walls[n - 1]
+    for i in range(n - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], walls[i])
+
+    total_water = 0
+    for i in range(n):
+        total_water += min(left_max[i], right_max[i]) - walls[i]
+
+    return total_water
+
+# Test cases
+if __name__ == "__main__":
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))
---
+++ rain/0_main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0_main
+"""
+rain = __import__('0-rain').rain
+
+if __name__ == "__main__":
+    walls = [0, 1, 0, 2, 0, 3, 0, 4]
+    print(rain(walls))
+    walls = [2, 0, 0, 4, 0, 0, 1, 0]
+    print(rain(walls))
