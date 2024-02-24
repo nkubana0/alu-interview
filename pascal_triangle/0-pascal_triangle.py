42ddf3c9e47e72403850d31c9f79d7ca9e3d3a0b
nkubana0 <i.shema@alustudent.com>
2024-02-24 11:46:59 +0200

    pascal triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,28 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+
 def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the nth row
     """
     if n <= 0:
         return []
-
+    
     triangle = [[1]]
 
     for i in range(1, n):
         row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        for j in range(1,i):
+            row.append(triangle[i - 1][j - 1]+ triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
 
     return triangle
 
-
+#test
 if __name__ == "__main__":
     def print_triangle(triangle):
         for row in triangle:
